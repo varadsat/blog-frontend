d63fb780ab0b165c960870f91eb94ed08097b87e
varad <varadsoham555@gmail.com>
2022-04-07 20:16:37 +0530

    Changed localhost port

--- src/appData.js
+++ src/appData.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === "development"? "https://localhost:5000" : "example.com";
+export const API_URL = process.env.NODE_ENV === "development"? "https://localhost" : "example.com";
